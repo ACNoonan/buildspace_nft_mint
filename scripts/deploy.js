296a1b5d5c0ed0626399973489b1a21cc3f19af8
Adam Noonan <adamcnoonan@gmail.com>
2022-02-07 13:10:04 -0500

    minor updates

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
@@ -9,11 +9,6 @@ const main = async () => {
     // Wait for it to be mined.
     await txn.wait()
     console.log("Minted NFT #1")
-  
-   //txn = await nftContract.makeAnEpicNFT()
-   //// Wait for it to be mined.
-   //await txn.wait()
-   //console.log("Minted NFT #2")
   };
   
   const runMain = async () => {
